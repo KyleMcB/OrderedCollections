6d424e9a60b04e441f9d0d257a63cfa1aed8e86e
Kyle Mcburnett <kylemcburnett@xingpeds.com>
2021-11-04 14:23:10 -0700

    trying to get jitpack to work

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,35 @@
 plugins {
+    `java-library`
+    id("org.jetbrains.kotlin.libs.publisher") version "0.0.60-dev-32"
     kotlin("jvm") version "1.5.31"
     `maven-publish`
 }
 
-group = "com.github.KyleMcB"
+group = "com.xingpeds.orderedcollections"
 version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    mavenLocal()
+}
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("OrderedCollections") {
+                from(components["java"])
+                groupId = "com.github.KyleMcB"
+                artifactId = "orderedcollections"
+            }
+        }
+    }
+    repositories {
+        mavenLocal()
+    }
 }
 
+publishing {
+
+}
 dependencies {
     implementation(kotlin("stdlib"))
     testImplementation(kotlin("test"))
