aab182e3f87838a2c8c40a86ec0400dc41a924f5
Kyle Mcburnett <kylemcburnett@xingpeds.com>
2021-11-12 08:44:35 -0800

    basicorderedmap maintainsortadd failing

--- OrderedCollectionTests/src/main/kotlin/testsuite/map/MutableOrderedMapTests.kt
+++ OrderedCollectionTests/src/main/kotlin/testsuite/map/MutableOrderedMapTests.kt
@@ -4,14 +4,22 @@ import orderedCollection.map.MutableOrderedMap
 import org.junit.jupiter.api.Test
 import kotlin.test.assertContentEquals
 
-interface MutableOrderedMapTests<K, V> {
+interface MutableOrderedMapTests<K : Comparable<K>, V> {
     val map: MutableOrderedMap<K, V>
     val values: Sequence<Pair<K, V>>
+
 
     @Test
     fun add() {
         val item = values.take(1).first()
         map.add(item)
         assertContentEquals(listOf(item), map)
+    }
+
+    @Test
+    fun maintainSortAdd() {
+        val data = values.take(100).shuffled().toList()
+        data.forEach { map.add(it) }
+        assertContentEquals(data.sortedBy { it.first }, map)
     }
 }
